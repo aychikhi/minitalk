3503419026e57a90e54e46b7a84f634bd11ca162
aychikhi <amigosayahe@gmail.com>
2025-02-13 11:39:52 +0100

    finish client part

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: aychikhi <aychikhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 11:21:53 by aychikhi          #+#    #+#             */
-/*   Updated: 2025/02/13 11:36:45 by aychikhi         ###   ########.fr       */
+/*   Updated: 2025/02/13 11:39:39 by aychikhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int main()
 	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
-	sigaction(SIGUSR1, )
+	// sigaction(SIGUSR1, )
 }
